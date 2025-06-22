1e6af3fdbd7fe9f00713191e083027feb42a1945
2bnt2 <yerandyty@gmail.com>
2025-06-22 08:57:42 -0500

    Update ReWrite.lua

--- ReWrite.lua
+++ ReWrite.lua
@@ -1,8 +1,1 @@
-game.Workspace.Model:Destroy()
-
-task.wait(1)  
-if game.Workspace:FindFirstChild("Model") then
-  print("Not Destroyed")
-else
-  print("Map Destroyed || ggs")
-end
+print("Hello World")
